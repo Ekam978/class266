f67a293835b84808ea9524623bbe625c4064fa25
Ekamjot Kaur <virk81gurpreet@gmail.com>
2022-11-25 18:49:48 +0530

    first commit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import cv2
 from flask import Flask, render_template, request, redirect, url_for, send_file
 from werkzeug.utils import secure_filename
-from PIL import Image
+from PIL import Image, ImageOps
 import numpy as np
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def upload_form():
-    return render_template('upload.html')
+	return render_template('upload.html')
 
 
 @app.route('/', methods=['POST'])
@@ -20,16 +20,26 @@ def upload_image():
 	filename = secure_filename(str(image))
 	image.save(os.path.join('static/', filename))
 	image_file = Image.open(image)
+	
 	rotated_image = image_file.rotate(degree)
 	rotated_image.save(os.path.join('static/', 'rotated_image.jpg'))
-	name = 'rotated_image.jpg'
+	get = request.form['tint']
+
+	if get != '':
+		gray = Image.open(os.path.join('static/', 'rotated_image.jpg')).convert('L')
+		rotated_image_edit = ImageOps.colorize(gray, black=get, white='white')
+	else:
+		print('Select an option')
+
+	rotated_image_edit.save(os.path.join('static/', 'rotated_image_edited.jpg'))
+	name = 'rotated_image_edited.jpg'
 	return render_template('upload.html', filename=name)
 
 
 @app.route('/display/<filename>')
 def display_image(filename):
-    return redirect(url_for('static', filename=filename))
+	return redirect(url_for('static', filename=filename))
 
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
